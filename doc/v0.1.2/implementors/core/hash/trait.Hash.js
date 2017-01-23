9c1b5ec116a17c6f0ea5b32da694f0d166ee2675
Hyunkang Kim <kimhyunkang@gmail.com>
2017-01-23 11:16:13 -0800

    Release v0.1.2

---
+++ doc/v0.1.2/implementors/core/hash/trait.Hash.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["immutable_map"] = ["impl&lt;T:&nbsp;<a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/hash/trait.Hash.html' title='core::hash::Hash'>Hash</a> for <a class='enum' href='immutable_map/enum.Bound.html' title='immutable_map::Bound'>Bound</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
